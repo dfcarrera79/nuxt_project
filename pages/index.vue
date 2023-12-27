793059ec3106dae6ff1acdf49d81371747b131b6
dfcarrera79 <44879886+dfcarrera79@users.noreply.github.com>
2023-12-26 21:07:33 -0500

    Quita comentarios del codigo

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,4 @@
-<script setup lang="ts">
-import { useLocalStore } from "../stores/index";
-import { storeToRefs } from "pinia";
-
-const localStore = useLocalStore();
-
-// const { count } = storeToRefs(localStore);
-// const { increment } = localStore;
-// const { decrement } = localStore;
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <q-page class="column justify-center content-center">
@@ -20,12 +11,6 @@ const localStore = useLocalStore();
         Producto desarrollado por Loxasoluciones bajo la dirección de Diego
         Fernando Carrera
       </p>
-
-      <!-- <p>You have clicked the button {{ count }} times.</p>
-      <div class="row q-gutter-md justify-center">
-        <q-btn round color="primary" icon="add" @click="increment" />
-        <q-btn round color="primary" icon="remove" @click="decrement" />
-      </div> -->
 
       <p>
         <a href="https://wa.me/593986704083" target="_blank">0986704083</a> -
